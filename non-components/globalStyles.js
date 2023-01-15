a56ff436357415e1f2e9c61d87348fca31bf1186
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-01-15 11:15:23 +0300

    Add colors in globalStyles.js

--- non-components/globalStyles.js
+++ non-components/globalStyles.js
@@ -1,9 +1,38 @@
 import { StyleSheet } from "react-native";
 
+/*
+    ? Fonts
+    
+    * Work Sans - Titles
+    * Nunito - paragraph text
+    * Roboto - tab navigation text
+*/
+
+const borderRadius = {
+    parent: 8,
+    child: 5,
+    tabNavbar: 12
+};
+
+const colors = {
+    // * Dark mode 
+    darkModeBackground: 'hsla(260, 4%, 15%, 1.0)', // #252427
+    darkGreyPurple: 'hsla(270, 4%, 22%, 1.0)', // #373539 
+    grey: 'hsla(0, 0%, 80%, 1.0)', //#CBCBCB
+
+    // * Light mode
+    lightModeBackground: 'hsla(30, 17%, 95%, 1.0)', // #F5F3F1
+    darkBrown: 'hsla(342, 30%, 8%, 1.0)', // #1C0F13
+    translucentDarkBrown: 'hsla(342, 30%, 8%, 0.45)', // #1C0F13
+
+    // * Shared
+    white: 'hsla(0, 0%, 100%, 1.0),' // #FFFFFF
+};
+
 const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightsalmon',
+        backgroundColor: colors.lightModeBackground,
         padding: 30
     }
 });
