11e8005c8195601b96ba626d20f0428536d698c6
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-01-15 10:34:59 +0300

    Add globalStyles.js

---
+++ non-components/globalStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lightsalmon',
+        padding: 30
+    }
+});
+
+export default globalStyles;
