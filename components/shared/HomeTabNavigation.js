9e0e49585749571688fc40c166b36897864fa902
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-01-16 13:32:56 +0300

    Initial tab navbar styling

--- components/shared/HomeTabNavigation.js
+++ components/shared/HomeTabNavigation.js
@@ -1,19 +1,47 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+
 import HomeScreen from "../screens/HomeScreen";
 import RecipesScreen from "../screens/RecipesScreen";
 import CookingScreen from "../screens/CookingScreen";
 import SettingsScreen from "../screens/SettingsScreen";
+import { faLightbulb } from "@fortawesome/free-regular-svg-icons";
+import { faFireBurner, faGear, faListCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Tab = createBottomTabNavigator();
 
 function HomeTabNavigation() {
     return ( 
-        <Tab.Navigator>
+        <Tab.Navigator 
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarIcon: ({ focused, color, size }) => {
+                    let icon;
+
+                    if (route.name === 'Feed') 
+                        icon = faLightbulb;
+
+                    else if (route.name === 'Recipes')
+                        icon = faListCheck;
+                    
+                    else if (route.name === 'Cooking')
+                        icon = faFireBurner;
+
+                    else 
+                        icon = faGear;
+
+                    return <FontAwesomeIcon icon={ icon } size={ 15 } color={ focused ? 'red' : 'black' } />
+                }                
+            })}
+        >
             <Tab.Screen name="Feed" component={ HomeScreen }/>
-            <Tab.Screen name="Recipes" component={ RecipesScreen }/>
-            <Tab.Screen name="Cooking" component={ CookingScreen }/>
-            <Tab.Screen name="Settings" component={ SettingsScreen }/>
+
+            <Tab.Screen name="Recipes" component={ RecipesScreen } />
+
+            <Tab.Screen name="Cooking" component={ CookingScreen } />
+
+            <Tab.Screen name="Settings" component={ SettingsScreen } />
         </Tab.Navigator>
     );
 }
