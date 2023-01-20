c0ea9fefa86cf1d4450a6d0203d2900af18300cb
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-01-20 17:05:31 +0300

    Add initial SettingsScreen components

--- components/screens/SettingsScreen.js
+++ components/screens/SettingsScreen.js
@@ -1,12 +1,34 @@
-import globalStyles from "../../non-components/globalStyles";
-import { Text, View } from "react-native";
+import globalStyles, { colors } from "../../non-components/globalStyles";
+
+import React, { useState } from 'react';
+
+import { Text, View, Switch } from "react-native";
 
 function SettingsScreen() {
+    const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+
     return ( 
-        <View>
-            <Text style={ globalStyles.lightModeTitleThin }>
-                This will be the settings screen.
-            </Text>
+        <View style={{
+            ...globalStyles.container,
+            alignItems: 'flex-start'
+        }}>
+            <View style={{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}>
+                <Text style={ globalStyles.cardTitleSmall }>
+                    Dark Mode
+                </Text>
+
+                <Switch 
+                    // thumbColor={ colors.white }
+                    // trackColor={{ false: 'grey' , true: colors.darkBrown }}
+                    value={ darkModeEnabled }
+                    onValueChange={ () => setDarkModeEnabled(!darkModeEnabled) }
+                />
+            </View>
         </View>
     );
 }
